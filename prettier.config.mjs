e3d13ea8f9c7e7bdce6c2bb0bbd459b898ff5ff5
AlbertLin <xieqideshu@gmail.com>
2024-07-19 13:42:00 +0800

    chore: 升级prettier插件

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,7 +3,6 @@ import prettier from '@mango-scripts/esp-config/prettier'
 /** @type {import("prettier").Config} */
 const config = {
   ...prettier,
-  plugins: ['prettier-plugin-tailwindcss'],
 }
 
 export default config
